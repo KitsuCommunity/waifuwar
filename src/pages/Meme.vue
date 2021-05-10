a36369045523d6c22c9247e30f3ec5071ef273a9
antoinejoly <jolyantoine@zoho.com>
2021-05-10 11:55:56 +0200

    Initial commit

---
+++ src/pages/Meme.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="columns is-multiline">
+    <div v-for="meme in memes" class="column meme" :key="meme.id">
+      <img :src="'http://localhost:3000/' + meme.image" />
+      <p>{{ meme.title }}</p>
+    </div>
+  </div>
+</template>
+<script>
+import { FETCH_MEME_QUERY } from "../gql/meme";
+export default {
+  data() {
+    return {
+      memes: [],
+    };
+  },
+  apollo: {
+    memes: {
+      query: FETCH_MEME_QUERY,
+      update(data) {
+        return data["meme"];
+      },
+    },
+  },
+};
+</script>
+<style lang="scss">
+.meme {
+  background-color: var(--foreground-background-color);
+  border-radius: 5px;
+  padding: 10px;
+}
+</style>
