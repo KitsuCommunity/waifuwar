23e13bf1e4e56e7b44458c9d3f7ea0d3fcc2dd41
Antoine Joly <jolyantoine@pm.me>
2021-06-28 10:23:54 +0200

    Update mutation_vote.js

--- src/gql/mutation_vote.js
+++ src/gql/mutation_vote.js
@@ -1,12 +1,11 @@
 import gql from 'graphql-tag'
 export const VOTE_MUTATION = gql`
-mutation submitVote($token: String!, $oppid: Int!, $ip: String!, $roundid: Int!) {
+mutation submitVote($token: String!, $oppid: Int!, $roundid: Int!) {
   submitVote(
     input: {
       token: $token
       oppId: $oppid
       roundId: $roundid
-      ip: $ip
     }
   )
   {
